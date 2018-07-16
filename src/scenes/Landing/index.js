a6d7a7194ea7867b89f17a9ffe89fd8080afb917
Mrls94 <sebasmrls94@gmail.com>
2018-07-15 22:12:18 -0500

    Create header container

--- src/scenes/Landing/index.js
+++ src/scenes/Landing/index.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
+import Header from '../../containers/Header'
 import logo from '../../logo.svg'
 
 class Landing extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React  HEllO LAURA 22222</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1 className='App-title'>Welcome to React  HEllO LAURA 22222</h1>
+          </header>
+          <p className='App-intro'>
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     )
   }
